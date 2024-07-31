a60031a8ae501412998122a5e6b2809bf3448cc9
Mariano Jimenez <104471721+Marianoj1991@users.noreply.github.com>
2024-07-31 09:33:27 -0300

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const counter = 0;
+let counter = 0;
 
 export const pool = new Pool({
   connectionString: process.env.POSTGRES_URL
@@ -32,5 +32,4 @@ async function createDatabaseAndTable() {
 }
 if(!counter) {
   createDatabaseAndTable();
-  counter = 1
 } 
